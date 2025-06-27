8e41b18e974b59bf491ee9824b3c4fc09f848294
aaravmajumdar <86802015+aaravmajumdar@users.noreply.github.com>
2025-06-27 16:57:39 +0530

    Delete exer1.py

--- exer1.py
+++
@@ -1,6 +0,0 @@
-# Create a dictionary and take input from the user and return the meaning of the word from the dictionary
-
-key : apple 
-value : A fruit that is typically red, green, or yellow and is sweet and crisp.
-
->> value 
